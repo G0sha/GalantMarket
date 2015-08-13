c220a1f7f64ab623261f4cea6c38706e925d1a21
Igor <gg0sha@ya.ru>
2015-08-13 23:21:20 +0300

    Add Store Page

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ActiveRecord::Base
     presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :subprice, numericality: {greater_than_or_equal_to: 0.01}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
