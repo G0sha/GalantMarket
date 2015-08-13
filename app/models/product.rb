e46d48a7411621408019e07a209b8f09db15f23e
Igor <gg0sha@ya.ru>
2015-08-13 22:58:43 +0300

    Validates Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
-   mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
+  validates :title, uniqueness: true
+  validates :title, :group, :subgroup, :description, :price, :subprice,
+    presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :subprice, numericality: {greater_than_or_equal_to: 0.01}
 end
