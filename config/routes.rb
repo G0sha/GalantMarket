6c1094f90725f909fe1e2d5b6d9f00ea36d48003
Igor <gg0sha@ya.ru>
2015-08-15 01:26:47 +0300

    Add Order system

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  resources :orders
   root 'store#index'
   root 'store#index', as: 'store'
   get 'store/index'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   resources :line_items
   resources :carts
