75c8a6103d4e188f7780e7d38580c0b9f04b48a5
Igor <gg0sha@ya.ru>
2015-08-14 20:39:20 +0300

    Add basic Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,27 @@
 class Product < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   validates :title, uniqueness: true
-  validates :title, :group, :subgroup, :description, :price, :subprice,
+  validates :title, :description, :price, :subprice,
     presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :subprice, numericality: {greater_than_or_equal_to: 0.01}
 
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+  # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
   end
 end
